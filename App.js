93d3da094c675bd879d97c0d7716457fa899f3a3
Komal <komalbishnoi87@gmail.com>
2025-03-14 16:55:53 +0530

    Apply css

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
-const heading = React.createElement("h1", {}, "This is Hello World from React");
+const heading = React.createElement("h1", {id : "heading"}, "This is Hello World from React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
